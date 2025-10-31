5b77f52ccaa521062be25d01a3f70ed92309264c
Lillian Osorio <lillianguadalupeosorio@gmail.com>
2025-10-30 19:52:56 -0600

    front transiciones y carousel lili

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 
         <!-- Texto -->
         <div class="col-lg-5 mb-4 mb-lg-0">
-          <h2 class="fw-bold text-conacica-green border-start border-conacica-green border-5 ps-3">Nosotros</h2>
+          <h2 class="fw-bold border-start border-conacica-green border-5 ps-3">Nosotros</h2>
           <p class="lead mt-3">
             Realizamos acciones en apoyo al fortalecimiento y profesionalización del sector Agroalimentario, 
             mediante la creación de vínculos con las Centrales de Abasto, Transportistas, Productores Agrícolas 
@@ -183,7 +183,9 @@
               <h4 class="fw-bold">Conexión Estratégica</h4>
               <p>Conéctate con mayoristas de todo el país.</p>
               <a href="ruta/a/directorio.html" class="btn btn-outline-warning mt-3">Directorio Mayorista</a>
+              
             </div>
+            
           </div>
         </div>
 
@@ -239,6 +241,27 @@
     </div>
 </section>
 
+<section>
+<div class="container text-center">
+      <h3 class="fw-bold mb-1">La Viña de San Pedro</h3>
+      <p class="text-muted mb-2">Conoce nuestro restaurante</p>
+      <div class="d-flex align-items-center">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3758.256826925522!2d-99.00659422478094!3d19.61631488170269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ef2f6b102cf9%3A0xe19ee0d43d2a0d4!2sRestaurante%20LA%20VI%C3%91A%20DE%20SAN%20PEDRO!5e0!3m2!1ses-419!2smx!4v1761856734128!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+      <div class="contenedor-imagen position-relative">
+        <img src="img/viña.JPG" alt="" width="600" height="450" class="">
+
+          <div class="overlay position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center">
+              <p class="text-white text-center p-3">
+                  Restaurante La Viña de San Pedro*<br>
+              </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</section>
+
   <!-- SECCIÓN: BLOG -->
 <section class="container py-2">
   <div class="text-center mb-2">
@@ -248,7 +271,48 @@
 
   </div>
 
-  <div id="carouselBlog" class="carousel slide">
+  
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/f1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>💀🌼 Día de Muertos: una tradición que da vida a la memoria de México 🇲🇽✨</h5>
+        <p>En México, el Día de Muertos es mucho más que una fecha… es un encuentro entre el pasado y el presente, entre los vivos y quienes ya partieron</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>COMUNICADO</h5>
+        <p>Nos unimos en duelo por la pérdida del empresario naranjero Javier Vargas Arias, en Álamo Temapache, Veracruz.</p>
+        <p>Desde CONACICA, expresamos nuestra profunda consternación y solidaridad con la familia, compañeros productores y con toda la comunidad citrícola de Veracruz.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/f3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5> La unión hace la fuerza en Veracruz </h5>
+        <p>En un acto de solidaridad, Gota De Miel Bodega, CONACICA y diversos donadores se unieron para llevar despensas y apoyo alimentario a las familias afectadas por las fuertes lluvias e inundaciones en el estado de Veracruz. </p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+  <div id="carouselBlog" class="carousel slide m-4">
     <div class="carousel-inner">
 
       <!-- Grupo 1 -->
@@ -343,35 +407,6 @@
   </div>
 </section>
 
-
-  <!-- MODAL LOGIN -->
-  <div class="modal fade" id="modalLogin" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content border-0 shadow login-modal">
-        <form action="./validaciones/loginVal.php" method="POST" class="p-4 rounded">
-          <div class="text-center mb-3">
-            <img src="logo.png" width="150" height="150" class="img-fluid mb-2">
-            <h5 class="fw-bold text-primary-custom">Iniciar sesión</h5>
-          </div>
-
-          <div class="mb-3">
-            <label for="user" class="form-label fw-semibold">Correo electrónico</label>
-            <input type="text" name="user" id="user" class="form-control form-control-lg input-custom" placeholder="Usuario" required>
-          </div>
-
-          <div class="mb-3">
-            <label for="password" class="form-label fw-semibold">Contraseña</label>
-            <input type="password" name="pwd" id="password" class="form-control form-control-lg input-custom" placeholder="••••••••" required>
-          </div>
-
-          <div class="d-flex justify-content-end gap-2">
-            <button type="button" class="btn btn-secondary-custom" data-bs-dismiss="modal">Cancelar</button>
-            <button type="submit" class="btn btn-primary-custom">Ingresar</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
   <!-- FOOTER -->
   <footer id="contacto" class="bg-green text-white pt-5 pb-3">
